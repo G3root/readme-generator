9d14a54a48da73fde0575ff15be8c2d72d289659
nafees nazik <nafeesnazik10@gmail.com>
2022-03-08 20:32:10 +0530

    add markdown preview css

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
 import "~/styles/globals.css";
+import "~/styles/markdown-color.css";
+import "~/styles/markdown.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "next-themes";
 
