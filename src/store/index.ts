7126c094618d5b6ea985ce1edc3b51f177c7af9b
nafees nazik <nafeesnazik10@gmail.com>
2022-03-08 20:23:35 +0530

    add markdown derived atom

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,7 @@
+import { atom } from "jotai";
 import { BlockValuesObject, BlocksObjectWithId } from "~/types";
 import { atomWithImmer } from "jotai/immer";
-import { generateBlockData } from "~/utils";
+import { generateBlockData, GenerateMarkdown } from "~/utils";
 
 const { nextId, inActiveBlocks, blockValues, defaultBlocks } =
   generateBlockData();
@@ -11,3 +12,11 @@ export const nextIdAtom = atomWithImmer<Number>(nextId);
 export const blockValuesAtom = atomWithImmer<BlockValuesObject>(blockValues);
 export const defaultBlocksAtom =
   atomWithImmer<BlocksObjectWithId>(defaultBlocks);
+
+export const markdownAtom = atom((get) => {
+  const blockIds = get(activeBlocksAtom);
+  const blocks = get(defaultBlocksAtom);
+  const blockValues = get(blockValuesAtom);
+  const markdown = GenerateMarkdown({ blockIds, blocks, blockValues });
+  return markdown;
+});
