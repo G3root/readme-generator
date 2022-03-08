24ed9852a1d5551144c0dd9dbc907aebf4a2654c
nafees nazik <nafeesnazik10@gmail.com>
2022-03-08 20:27:57 +0530

    add raw tab panel

---
+++ src/components/editor/PreviewColumnTab/RawTabPanel.tsx
@@ -0,0 +1,37 @@
+import * as React from "react";
+import Highlight, { defaultProps } from "prism-react-renderer";
+import theme from "prism-react-renderer/themes/nightOwl";
+import { clsx } from "~/utils";
+import { useAtomValue } from "jotai";
+import { markdownAtom } from "~/store";
+
+export interface IRawTabPanelProps {}
+
+export function RawTabPanel(props: IRawTabPanelProps) {
+  const markdown = useAtomValue(markdownAtom);
+  return (
+    <div className="h-[83vh] overflow-auto rounded-lg bg-[#011627] p-5">
+      <Highlight
+        {...defaultProps}
+        code={markdown}
+        theme={theme}
+        language="markdown"
+      >
+        {({ className, style, tokens, getLineProps, getTokenProps }) => (
+          <pre className={className} style={style}>
+            {tokens.map((line, i) => {
+              const { className, ...rest } = getLineProps({ line, key: i });
+              return (
+                <div key={i} className={clsx(className, "px-5")} {...rest}>
+                  {line.map((token, key) => (
+                    <span key={key} {...getTokenProps({ token, key })} />
+                  ))}
+                </div>
+              );
+            })}
+          </pre>
+        )}
+      </Highlight>
+    </div>
+  );
+}
