11bcaf419df62c9e0367444aaa8ba063ad34dfbe
nafees nazik <nafeesnazik10@gmail.com>
2022-03-09 14:45:14 +0530

    add tool tip component

---
+++ src/components/primitives/ToolTip/ToolTip.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import { toolTipStyles, ToolTipStyleProps } from "./styles";
+
+export interface IToolTipProps extends ToolTipStyleProps {
+  value: string;
+}
+
+export function ToolTip({
+  direction,
+  className,
+  forceOpen,
+  scheme,
+  children,
+  value,
+  ...rest
+}: IToolTipProps) {
+  return (
+    <div
+      className={toolTipStyles({
+        direction,
+        forceOpen,
+        scheme,
+        class: className,
+      })}
+      data-tip={value}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+}
--- src/components/primitives/index.ts
+++ src/components/primitives/index.ts
@@ -6,3 +6,4 @@ export * from "./Select";
 export * from "./FormControl";
 export * from "./VisuallyHidden";
 export * from "./ButtonGroup";
+export * from "./ToolTip";
