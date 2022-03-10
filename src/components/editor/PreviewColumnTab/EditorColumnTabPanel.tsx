eea3bcf0ccb3145647fed0785858c5c24ad00bf6
nafees nazik <nafeesnazik10@gmail.com>
2022-03-10 11:51:09 +0530

    add editor column tab panel

---
+++ src/components/editor/PreviewColumnTab/EditorColumnTabPanel.tsx
@@ -0,0 +1,23 @@
+import { useAtomValue } from "jotai";
+import * as React from "react";
+import { DraggableCard } from "~/components/editor";
+import { activeBlocksAtom } from "~/store";
+
+import { SortableContext } from "@dnd-kit/sortable";
+
+export interface IEditorColumnTabPanelProps {}
+
+export function EditorColumnTabPanel(props: IEditorColumnTabPanelProps) {
+  const blockIds = useAtomValue(activeBlocksAtom);
+  return (
+    <SortableContext items={blockIds}>
+      <div className="h-[83vh] overflow-auto border p-4">
+        <ul className="flex flex-col space-y-2">
+          {blockIds.map((data, index) => (
+            <DraggableCard key={data} id={data} positon={index} />
+          ))}
+        </ul>
+      </div>
+    </SortableContext>
+  );
+}
