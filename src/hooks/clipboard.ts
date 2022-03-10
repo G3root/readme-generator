f4902299bf1a1d24fe3f960d8a6d3d7ddde65b0a
nafees nazik <nafeesnazik10@gmail.com>
2022-03-10 11:38:35 +0530

    add use clipboard hook

---
+++ src/hooks/clipboard.ts
@@ -0,0 +1,19 @@
+import copy from "copy-text-to-clipboard";
+import * as React from "react";
+
+export function useClipboard(timeout = 1500) {
+  const [isCopied, setIsCopied] = React.useState(false);
+
+  const handleCopy = (data: string) => {
+    copy(data);
+    setIsCopied(true);
+    setTimeout(() => {
+      setIsCopied(false);
+    }, timeout);
+  };
+
+  return {
+    isCopied,
+    handleCopy,
+  };
+}
