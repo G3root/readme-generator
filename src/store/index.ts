0436553c16d3cbe56fe67d523a37512804cf741e
nafees nazik <nafeesnazik10@gmail.com>
2022-03-10 21:09:40 +0530

    add atom for custom blocks

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,20 +5,41 @@ import { generateBlockData, GenerateMarkdown } from '~/utils'
 
 const { nextId, inActiveBlocks, blockValues, defaultBlocks } = generateBlockData()
 
+// atoms wich key of defaultBlocksAtom/customBlocksAtom which are active
 export const activeBlocksAtom = atomWithImmer<string[]>([])
+
+// atoms wich key of defaultBlocksAtom/customBlocksAtom which are active
 export const inActiveBlocksAtom = atomWithImmer<string[]>(inActiveBlocks)
+
+// key for customBlocksAtom which increments
 export const nextIdAtom = atomWithImmer<Number>(nextId)
+
+//atom which holds editable values of customBlocksAtom/defaultBlocksAtom
 export const blockValuesAtom = atomWithImmer<BlockValuesObject>(blockValues)
 
+// atoms wich controls modal states
 export const blockConfigModalStateAtom = atom(false)
 export const addItemsModalStateAtom = atom(false)
+export const customBlockModalStateAtom = atom(false)
 
+// atom wich holds default blocks
 export const defaultBlocksAtom = atomWithImmer<BlocksObjectWithId>(defaultBlocks)
 
+// atom wich holds custom blocks
+export const customBlocksAtom = atomWithImmer<BlocksObjectWithId>({})
+
+// a derived atom which generates markdown from block value which are active
 export const markdownAtom = atom((get) => {
   const blockIds = get(activeBlocksAtom)
   const blocks = get(defaultBlocksAtom)
   const blockValues = get(blockValuesAtom)
   const markdown = GenerateMarkdown({ blockIds, blocks, blockValues })
   return markdown
+})
+
+// a derived atom which returns combination of customBlocksAtom and defaultBlocksAtom
+export const allBlocks = atom((get) => {
+  const defaultBlocks = get(defaultBlocksAtom)
+  const customBlocks = get(customBlocksAtom)
+  return Object.assign(defaultBlocks, customBlocks)
 })
