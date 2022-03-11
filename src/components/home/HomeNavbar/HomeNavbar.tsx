a06252452f5eb0ac6aea4f9170f4bc0238db96ce
nafees nazik <nafeesnazik10@gmail.com>
2022-03-11 13:54:03 +0530

    remove unnecessary anchor tag

--- src/components/home/HomeNavbar/HomeNavbar.tsx
+++ src/components/home/HomeNavbar/HomeNavbar.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import { NavbarLogo, ThemeSwitcher } from '~/components/common'
-import Link from 'next/link'
 
 export interface IHomeNavbarProps {}
 
@@ -9,11 +8,7 @@ export function HomeNavbar(props: IHomeNavbarProps) {
     <div className="mx-auto h-16 max-w-screen-2xl">
       <div className="flex h-full w-full items-center justify-between px-6">
         <div>
-          <Link href="/">
-            <a>
-              <NavbarLogo />
-            </a>
-          </Link>
+          <NavbarLogo />
         </div>
         <div>
           <ThemeSwitcher />
