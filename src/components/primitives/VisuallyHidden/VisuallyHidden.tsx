dda1234ab9a24b5a8908251ce1f57d2b50e3c3b6
nafees nazik <nafeesnazik10@gmail.com>
2022-03-31 11:10:12 +0530

    remove unused files

--- src/components/primitives/VisuallyHidden/VisuallyHidden.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react'
-
-export interface IVisuallyHiddenProps {
-  children: React.ReactNode
-}
-
-export function VisuallyHidden({ children }: IVisuallyHiddenProps) {
-  return <div className="sr-only">{children}</div>
-}
