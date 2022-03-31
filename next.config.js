17e07d39549bebb1a34af8eb625bf5bf51f736bb
nafees nazik <nafeesnazik10@gmail.com>
2022-03-31 11:10:25 +0530

    remove preact in production

--- next.config.js
+++ next.config.js
@@ -4,19 +4,6 @@ const { i18n } = require('./next-i18next.config')
 const nextConfig = {
   reactStrictMode: true,
   i18n,
-  webpack: (config, { dev, isServer }) => {
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      })
-    }
-
-    return config
-  },
 }
 
 module.exports = nextConfig
